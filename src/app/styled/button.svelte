e701e00bd1e86ae767f47057ae80517402cac4ec
aron edwards <aronedwards91@gmail.com>
2023-02-05 09:20:09 +0000

    npc setup

---
+++ src/app/styled/button.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let onClick;
+</script>
+
+<button
+	on:click={onClick}
+	class="p-2 border-2 border-slate-800 bg-slate-700 bg-opacity-25 rounded hover:bg-opacity-30 transition-all"
+	><slot /></button
+>
--- src/routes/npcs/+page.svelte
+++ src/routes/npcs/+page.svelte
@@ -1,8 +1,90 @@
-<h1 class="text-4xl pb-4">NPC's Generator</h1>
+<script>
+	import Btn from '../../app/styled/button.svelte';
+	import TableGen from '@random-tables/generator-logic';
+	import FantasyTable from '@random-tables/npc-fantasy';
 
-<p>TODO...</p>
+	TableGen.buildIndex([FantasyTable]);
 
-<p>lorem ipsum text filler</p>
-<p>lorem ipsum text filler</p>
-<p class="prose">prose lorem ipsum text filler</p>
-<p class="prose">prose lorem ipsum text filler{'\n'}...lorem ipsum text filler</p>
+	let outputName;
+	let outputAge;
+	let outputDescription;
+	let outputPersonality;
+	let outputMotivation;
+	let outputFamily;
+	let outputPast;
+
+	async function triggerCall(call, button) {
+		button.disabled = true;
+		const npcData = await TableGen.getCall(call);
+
+		outputName.textContent = npcData[0].value;
+		outputAge.textContent = npcData[1].value;
+		outputDescription.textContent = npcData[2].value;
+		outputPersonality.textContent = npcData[3].value;
+		outputMotivation.textContent = npcData[4].value;
+		outputFamily.textContent = npcData[5].value;
+		outputPast.textContent = npcData[6].value;
+
+		button.disabled = false;
+	}
+
+	function dwarfMaleClick(evt) {
+		triggerCall('npc-fantasy/dwarf/male', evt.srcElement);
+	}
+	function dwarfFemaleClick(evt) {
+		triggerCall('npc-fantasy/dwarf/female', evt.srcElement);
+	}
+	function elfMaleClick(evt) {
+		triggerCall('npc-fantasy/elf/male-middle', evt.srcElement);
+	}
+	function elfFemaleClick(evt) {
+		triggerCall('npc-fantasy/elf/female-middle', evt.srcElement);
+	}
+	function highelfMaleClick(evt) {
+		triggerCall('npc-fantasy/elf/male-high', evt.srcElement);
+	}
+	function highelfFemaleClick(evt) {
+		triggerCall('npc-fantasy/elf/female-high', evt.srcElement);
+	}
+
+    const outputCSS = "border border-slate-300 rounded p-2 mb-2"
+</script>
+
+<h1 class="text-4xl mb-4 flex justify-center">NPC's Generator</h1>
+
+<h3 class="text-2xl mb-2">NPC's</h3>
+
+<p class="mb-2">Select one of the below NPC buttons to generate a character</p>
+
+<div class="flex flex-wrap gap-4 mb-6">
+	<Btn onClick={dwarfMaleClick}>Fantasy - Dwarf - Male</Btn>
+	<Btn onClick={dwarfFemaleClick}>Fantasy - Dwarf - Female</Btn>
+	<Btn onClick={elfMaleClick}>Fantasy - Elf - Male</Btn>
+	<Btn onClick={elfFemaleClick}>Fantasy - Elf - Female</Btn>
+	<Btn onClick={highelfMaleClick}>Fantasy - High Elf - Male</Btn>
+	<Btn onClick={highelfFemaleClick}>Fantasy - High Elf - Female</Btn>
+</div>
+
+<div class="flex mb-6 space-x-4 items-center">
+	<span class="font-bold">Name</span>
+	<span bind:this={outputName} class={outputCSS + " w-40"}>-</span>
+	<span class="font-bold">Age</span>
+	<span bind:this={outputAge} class={outputCSS + " w-12"}>-</span>
+</div>
+
+<div class="grid grid-cols-2 gap-4">
+	<div>
+		<span class="font-bold">Description</span>
+		<div bind:this={outputDescription} class={outputCSS} style="min-height: 4rem;">-</div>
+		<span class="font-bold">Personality</span>
+		<div bind:this={outputPersonality} class={outputCSS} style="min-height: 2rem;">-</div>
+		<span class="font-bold">Motivations</span>
+		<div bind:this={outputMotivation} class={outputCSS} style="min-height: 2rem;">-</div>
+	</div>
+	<div>
+		<span class="font-bold">Family</span>
+		<div bind:this={outputFamily} class={outputCSS} style="min-height: 4rem;">-</div>
+		<span class="font-bold">Past</span>
+		<div bind:this={outputPast} class={outputCSS} style="min-height: 2rem;">-</div>
+	</div>
+</div>
