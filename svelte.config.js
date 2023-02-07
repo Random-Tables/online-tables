e9671e386b7a7fe8036b51a9b75ac3bf98332216
aron edwards <aronedwards91@gmail.com>
2023-02-07 20:15:35 +0000

    set fallback

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,9 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: null,
+			fallback: 'index.html',
 			precompress: false,
+			router: false,
 			strict: true
 		})
 	},
